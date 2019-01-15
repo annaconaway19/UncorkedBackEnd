41cb871f58bf1d1e164d1fafeae2a8085a198fab
github username <github email address>
2019-01-15 17:57:13 -0500

    working on more pagination

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,32 +1,27 @@
 class CountriesController < ApplicationController
 
   def index
-    if params[:page] && params[:page][:number]
-      countries = Country.paginate(page: params[:page][:number], per_page: 10)
-      total_pages = (Country.count / 10).ceil
-      current_page = params[:page][:number]
-    else
-      countries = Country.all
-      current_page = 1
-    end
-
-    pagination = {
-      "current_page": current_page,
-      "last_page": total_pages,
-      "next_page_url": `http://localhost:3001/countries?page[number]=#{current_page + 1}`,
-      "prev_page_url": `http://localhost:3001/countries?page[number]=#{current_page - 1}`,
-    }
-
-    render json: {countries: countries, pagination: pagination}
+    # if params[:page] && params[:page][:number]
+    #   countries = Country.paginate(page: params[:page][:number], per_page: 10)
+    #   total_pages = (Country.count / 10).ceil
+    #   current_page = params[:page][:number]
+    # else
+    #   countries = Country.all
+    #   current_page = 1
+    # end
+    #
+    # pagination = {
+    #   "current_page": current_page,
+    #   "last_page": total_pages,
+    #   "next_page_url": `http://localhost:3001/countries?page[number]=#{current_page + 1}`,
+    #   "prev_page_url": `http://localhost:3001/countries?page[number]=#{current_page - 1}`,
+    # }
+    
+      render json: Country.all
   end
 
   def show
     render json: Country.find(params[:id])
-  end
-
-  def limit_show
-    params[:page]
-    @country.wines.slice(params[:page] - 1, +10)
   end
 
   private
