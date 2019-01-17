91fa7ef07bd2e02e6af2fe84ef00114c76aa0cdb
github username <github email address>
2019-01-17 15:05:34 -0500

    search fetch and pagination

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -2,18 +2,21 @@
 class WinesController < ApplicationController
 
   def index
-    byebug
-      paginate Wine.unscoped, per_page: 20
+      paginate Wine, per_page: 20
   end
 
   def search
     if params[:search]
-      render json: Wine.select { |w| w.name.include?(params[:search]) }
+      wines = Wine.select { |w| w.name.include?(params[:search]) }
+      # byebug
+      paginate Wine.where(id: wines.map(&:id)), per_page: 20
+
+      # paginate Wine.unscoped.select { |w| w.name.include?(params[:search]) }, per_page: 20
     end
   end
 
   def show
-    render json: Wine.find(params[:id]) #, includes:[ :wines]
+    render json: Wine.find(params[:id])
   end
 
   private
