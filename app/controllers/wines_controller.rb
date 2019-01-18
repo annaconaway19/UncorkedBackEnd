e1d21e98422629f30b49687a6eb0068b63c3a4f4
github username <github email address>
2019-01-18 10:16:27 -0500

    search finished

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -8,12 +8,11 @@ class WinesController < ApplicationController
   def search
    if params[:search]
      wines = Wine.select { |w| w.name.include?(params[:search]) }
-      if wines.empty?
-
-        paginate Wine, per_page: 20
-      else
+      # if wines.empty?
+      #   paginate Wine, per_page: 20
+      # else
         paginate Wine.where(id: wines.map(&:id)), per_page: 20
-      end
+      # end
    end
  end
 
